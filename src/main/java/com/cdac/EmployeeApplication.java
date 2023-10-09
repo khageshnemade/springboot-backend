778d5ad0f0d5791400ef85edaa0270c24277e83e
Khagesh <khageshnemade@gmail.com>
2023-10-09 16:26:15 +0530

    ForJenkins

--- src/main/java/com/cdac/EmployeeApplication.java
+++ src/main/java/com/cdac/EmployeeApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -12,19 +14,21 @@ import com.cdac.model.Employee;
 import com.cdac.repo.EmployeeRepo;
 
 @SpringBootApplication
-public class EmployeeApplication implements CommandLineRunner {
+public class EmployeeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
 
-	@Autowired private EmployeeRepo empRepo;
 	@Override
-	public void run(String... args) throws Exception {
-		//empRepo.save(new Employee("Khagesh","Nemade","khageshnemade@gmail.com"));
-//		empRepo.save(new Employee("sunil","Nemade","sunilnemade@gmail.com"));
-		
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(EmployeeApplication.class);
 	}
+
+
+
+	
 
 }
 
