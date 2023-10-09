c70722a2caa11250c1eb9fd4ee1ad185d7f80f47
Khagesh <khageshnemade@gmail.com>
2023-10-09 18:02:03 +0530

    AddAdd

---
+++ src/main/java/com/cdac/controller/FreeController.java
@@ -0,0 +1,15 @@
+package com.cdac.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class FreeController {
+@GetMapping
+    public String add(){
+        return "From Add COmponent of the COntroller";
+    }
+    
+}
